fb0eb3b45ebdb1b3f0083e57f096f9e14fe24516
Missbhawana <168270877+Missbhawana@users.noreply.github.com>
2024-04-28 20:23:25 +0530

    guess number game

---
+++ guess_number.py
@@ -0,0 +1,7 @@
+import random
+random_number = random.randint(1,10)
+guess = int(input("ENTER THE NUMBER: "))
+while(guess != random_number):
+  print("try again")
+  guess = int(input("ENTER THE NUMBER: "))
+print("RIGHT GUESS!!!!")
